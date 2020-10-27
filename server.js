812571731df84c4bd1fc4ad6bc67d43419e8064d
Luis <lgd26308@gmail.com>
2020-10-27 14:52:25 -0700

    updates

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+
+require("./routes/routes")(app);
+
 
 app.listen(PORT, function () {
-    console.log("App listening on PORT: " + PORT);
+    console.log("App listening on PORT, http://localhost:" + PORT);
 });

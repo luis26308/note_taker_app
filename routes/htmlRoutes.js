384d0dfa4d8d0c0c6f2d9b6f9f0ff927605a4895
Luis <lgd26308@gmail.com>
2020-10-28 17:01:46 -0700

    added get request at /

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,6 +6,10 @@ app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+});
+
 app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"))
 });
